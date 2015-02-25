221553b64830701a8d7949889b6b80dbfa03d02e
Cecilie Teisberg <cecilie.teisberg@gmail.com>
2015-02-25 12:16:28 +0100

    fixes

--- src/DB/Database.java
+++ src/DB/Database.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
-/** H�ndterer kommunikasjon mellom programmet og MySQL-databasen */
+/** Håndterer kommunikasjon mellom programmet og MySQL-databasen */
 public class Database {
 	public static boolean DEBUG = true;
 	private static String url = "jdbc:mysql://mysql.stud.ntnu.no/";
@@ -18,12 +18,12 @@ public class Database {
 	public static boolean initializeDatabase() {
 		try {
 			// Sletter alle tidligere tabeller 
-			makeStatement("DROP TABLE Person");
-//			makeStatement("DROP TABLE PersonInGroup");
-//			makeStatement("DROP TABLE Group");
+			makeStatement("DROP TABLE PersonInGroup");
+			makeStatement("DROP TABLE Person;");
+			makeStatement("DROP TABLE Group0");
+			makeStatement("DROP TABLE Room");
 //			makeStatement("DROP TABLE PartOfEvent");
 //			makeStatement("DROP TABLE Event");
-//			makeStatement("DROP TABLE Room");
 			
 			// Oppretter Person-tabellen
 			makeStatement("CREATE TABLE Person"
@@ -34,8 +34,8 @@ public class Database {
 					+ "PRIMARY KEY (PersonID));");
 			
 			//Oppretter Group-tabellen		
-			makeStatement("CREATE TABLE Group"
-					+ "(GroupID INT	NOT NULL AUTO_INCREMENT,"
+			makeStatement("CREATE TABLE Group0"
+					+ "(GroupID INT NOT NULL,"
 					+ "Name VARCHAR(20) NOT NULL,"
 					+ "PRIMARY KEY (GroupID));");
 			
@@ -44,10 +44,10 @@ public class Database {
 					+ "(PersonID INT NOT NULL,"
 					+ "GroupID INT NOT NULL,"
 					+ "PRIMARY KEY (PersonID, GroupID),"
-					+ "FOREIGN KEY (PERSONID) REFERENCES Person(PersonID),"
-					//+ "ON UPDATE CASCADE ON DELETE CASCADE,"
-					+ "FOREIGN KEY (GROUPID) REFERENCES Group(GroupID));");
-					//+ "ON UPDATE CASCADE ON DELETE CASCADE));");
+					+ "FOREIGN KEY (PersonID) REFERENCES Person(PersonID) "
+					+ "ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ "FOREIGN KEY (GroupID) REFERENCES Group0(GroupID) "
+					+ "ON UPDATE CASCADE ON DELETE CASCADE);");
 			
 			//Oppretter Room-tabellen
 			makeStatement("CREATE TABLE Room"
@@ -65,24 +65,24 @@ public class Database {
 					+ "Description VARCHAR(100),"
 					+ "RoomID INT,"
 					+ "PRIMARY KEY (EventID),"
-					+ "FOREIGN KEY (RoomID) REFERENCES ROOM(RoomID));");
-					//+ "ON UPDATE CASCADE));");
+					+ "FOREIGN KEY (RoomID) REFERENCES ROOM(RoomID) "
+					+ "ON UPDATE CASCADE);");
 			
 			//Oppretter PartOfEvent-tabellen
 			makeStatement("CREATE TABLE PartOfEvent"
 					+ "(ObjectID INT NOT NULL,"
 					+ "EventID INT NOT NULL,"
 					+ "PRIMARY KEY (ObjectID, EventID),"
-					+ "FOREIGN KEY (ObjectID) REFERENCES PERSON(PersonID), "
-					//+ "ON UPDATE CASCADE ON DELETE CASCADE, "
-					+ "FOREIGN KEY (EventID) REFERENCES EVENT(EventID));");
-					//+ "ON UPDATE CASCADE ON DELETE CASCADE);");
+					+ "FOREIGN KEY (ObjectID) REFERENCES Person(PersonID) "
+					+ "ON UPDATE CASCADE ON DELETE CASCADE, "
+					+ "FOREIGN KEY (EventID) REFERENCES Event(EventID) "
+					+ "ON UPDATE CASCADE ON DELETE CASCADE);");
 			
 			makeStatement("INSERT INTO Person\n"
 					+ "VALUES('1', 'Cecilie Teisberg', 'cecilite', 'passord');");
 			makeStatement("INSERT INTO Person\n"
 					+ "VALUES('2', 'Anders Rønold', 'anron', 'passord1');");
-			makeStatement("INSERT INTO Group(id, name)"
+			makeStatement("INSERT INTO Group0(id, name)"
 					+ "VALUES('3', 'PU');");
 			makeStatement("INSERT INTO PersonInGroup\n"
 					+ "VALUES('1', '3');");
@@ -97,17 +97,13 @@ public class Database {
 			return false;
 		}
 	}
-	
-	public static ResultSet doQuery(String query) {
-		return makeQuery(query);
-	}
 
 	/**
-	 * Gj�r en sp�rring mot databasen
+	 * Gjør en spørring mot databasen
 	 * @param query En query-streng
 	 * @return Et resultat-sett
 	 */
-	private static ResultSet makeQuery(String query) {
+	public static ResultSet makeQuery(String query) {
 		ResultSet res = null;
 		try {
 			Connection conn = getConnection();
@@ -123,16 +119,16 @@ public class Database {
 	}
 
 	/**
-	 * Utf�rer statements mot databasen
-	 * @param statement Statement som skal utf�res
-	 * @return returnerer om statementen ble fullf�rt
+	 * Utfører statements mot databasen
+	 * @param statement Statement som skal utføres
+	 * @return returnerer om statementen ble fullført
 	 */
 	private static boolean makeStatement(String statement) {
 		try {
 			Connection conn = getConnection();
 			Statement st = conn.createStatement();
-			/*int res = */st.executeUpdate(statement); //trenger denne å lagres??
-			conn.close();
+			int res = st.executeUpdate(statement); //trenger denne å lagres??
+			//conn.close();
 			return true;
 		} catch (Exception e) {
 			if (DEBUG)
