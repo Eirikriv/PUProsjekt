1d91e4c2e0b0cc4ebffbd854e39b88d029f04a73
Cecilie Teisberg <cecilie.teisberg@gmail.com>
2015-02-27 12:48:00 +0100

    update

--- src/Core/Calendar.java
+++ src/Core/Calendar.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
 import DB.DatabaseHandler;
 
 public class Calendar {
-	private String name;
-	ArrayList<Event> calendar = new ArrayList<Event>();
+	private Person owner;
+	private ArrayList<Event> calendar = new ArrayList<Event>();
 	
-	public Calendar(String name) {
-		this.name = name;
+	public Calendar(Person owner) {
+		this.owner = owner;
 	}
 	
 	public void addEvent(String name, String start, String end, String desc, String roomId) {
@@ -20,22 +20,25 @@ public class Calendar {
 		this.calendar.add(e);
 	}
 	
-	public ArrayList<Event> outputPersonEvents() throws SQLException {
-		ArrayList<ArrayList<String>> list = DatabaseHandler.getPersonEvents(name);
+	public void makeCalendar() throws SQLException {
+		ArrayList<ArrayList<String>> list = DatabaseHandler.getPersonEvents(owner.getName());
 		ArrayList<Event> result = new ArrayList<Event>();
 		for (int i=0; i<list.size() ; i++) {
-				String name = list.get(i).get(0);
-				String start = list.get(i).get(1);
-				String end = list.get(i).get(2);
-				String desc = list.get(i).get(3);
-				String id = list.get(i).get(4);
-				
-				Event e = new Event(name, start, end, desc, id);
-				result.add(e);
-			}
-		return result;
-		
+			String name = list.get(i).get(0);
+			String start = list.get(i).get(1);
+			String end = list.get(i).get(2);
+			String desc = list.get(i).get(3);
+			String id = list.get(i).get(4);
+			
+			Event e = new Event(name, start, end, desc, id);
+			result.add(e);
 		}
+		this.calendar = result;
+	}
+	
+	public ArrayList<Event> getCalendar() {
+		return calendar;
+	}
 		
 }
 	
