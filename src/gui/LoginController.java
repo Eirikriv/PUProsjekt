5c214c957aa2ebbd280c505b3e5f8b85076b06e1
reaVen <reaVen@10.0.0.4>
2015-03-03 18:08:13 +0100

    La til feilmelding på login skjermen

--- src/gui/LoginController.java
+++ src/gui/LoginController.java
@@ -10,10 +10,10 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class LoginController implements Initializable {
 	
@@ -21,7 +21,7 @@ public class LoginController implements Initializable {
 	@FXML private PasswordField passwordText;
 	@FXML private Button newButton;
 	@FXML private Button loginButton;
-	@FXML private VBox loginContainer;
+	@FXML private Text loginLabel;
 	
 	PersonDatabaseHandler pdb = new PersonDatabaseHandler();
 
@@ -34,12 +34,15 @@ public class LoginController implements Initializable {
         
         loginButton.setOnAction(new EventHandler<ActionEvent>() {
         	@Override public void handle(ActionEvent event) {
-        		ArrayList<String> person = pdb.login(usernameText.getText(), passwordText.getText());
-        		if (person.size() > 0) {
-        			SessionData.username = person.get(0);
-        			ScreenNavigator.loadVista(ScreenNavigator.SCREEN_CALENDAR);
-        		} else {
-        			//loginContainer.getChildren().add(new Label("Login failed"));
+        		try {
+	        		ArrayList<String> person = pdb.login(usernameText.getText(), passwordText.getText());
+	        		if (person.size() > 0) {
+	        			SessionData.username = person.get(0);
+	        			ScreenNavigator.loadVista(ScreenNavigator.SCREEN_CALENDAR);
+	        		}
+        		} catch (Exception e) {
+        			loginLabel.setText("Login failed..");
+        			loginLabel.setFill(Color.RED);
         		}
         	}
         });
