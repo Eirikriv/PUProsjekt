bb8fba15c4d38bfcb153db28e4d5135400e0ed3a
dritt <dritt@78.91.41.109>
2015-03-04 12:33:34 +0100

    asd

--- src/gui/AppointmentController.java
+++ src/gui/AppointmentController.java
@@ -3,6 +3,10 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
@@ -15,29 +19,44 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
-public class appointmentController implements Initializable {
+public class AppointmentController implements Initializable {
 	
 	@FXML private GridPane appointmentContainer;
 	@FXML private TextField titleField;
 	@FXML private DatePicker dateField;
 	@FXML private TextField startField;
 	@FXML private TextField endField;
-	@FXML private ListView<VBox> listMembersField;
+	@FXML private ListView<String> listMembersField;
 	@FXML private Button searchRoomsButton;
+	
+	private ObservableList<String> listViewList = FXCollections.observableArrayList();
 	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		appointmentContainer.add(new FilterComboBox(SessionData.allMembers), 1, 3);
-		appointmentContainer.add(new FilterComboBox(SessionData.allGroups), 1, 4);
+		FilterComboBox members = new FilterComboBox(SessionData.allMembers);
+		FilterComboBox groups = new FilterComboBox(SessionData.allGroups);
+		FilterComboBox rooms = new FilterComboBox(SessionData.allRooms);
+		listMembersField.setItems(listViewList);
+		
+		members.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			public void changed(ObservableValue<? extends String> observable,
+					String oldValue, String newValue) {
+					addToListView(newValue);
+					System.out.println(newValue);
+					System.out.println(listViewList.size());
+				}
+		});
+		
+		appointmentContainer.add(members, 1, 3);
+		appointmentContainer.add(groups, 1, 4);
 		
 		StackPane hb = new StackPane();
 		HBox sp = new HBox();
 		TextField tf = new TextField();
-		FilterComboBox fcb = new FilterComboBox(SessionData.allRooms);
-		sp.getChildren().addAll(tf, fcb);
+		
+		sp.getChildren().addAll(tf, rooms);
 		hb.getChildren().add(sp);
 		sp.setAlignment(Pos.CENTER);
 		appointmentContainer.add(hb, 1, 5);
@@ -50,6 +69,10 @@ public class appointmentController implements Initializable {
 	
 	public void keyStroke(KeyEvent e) {
 		System.out.println(e.getCharacter());
+	}
+	
+	public void addToListView(String item) {
+		this.listViewList.add(item);
 	}
 
 }
