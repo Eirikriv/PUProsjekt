904443ce3db67672085d000428477a3759ede93e
dritt <dritt@78.91.41.156>
2015-03-05 12:40:15 +0100

    fiksa sånn at jeg får opp gruppene jeg er med i - i GUIet

--- src/gui/LoginController.java
+++ src/gui/LoginController.java
@@ -33,8 +33,6 @@ public class LoginController implements Initializable {
         assert loginButton != null : "fx:id=\"loginbutton\" was not injected: check your FXML file 'LoginScreen.fxml'.";
         
         loginButton.setDefaultButton(true);
-        
-        
         loginButton.setOnAction(new EventHandler<ActionEvent>() {
         	@Override public void handle(ActionEvent event) {
         		try {
@@ -52,7 +50,6 @@ public class LoginController implements Initializable {
         
         newButton.setOnAction(new EventHandler<ActionEvent>() {
         	@Override public void handle(ActionEvent event) {
-        		System.out.println("Clicked new button");
         	}
         });
 		
