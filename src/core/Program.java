946eade7a8758960980e3191d154701c43058909
Cecilie Teisberg <cecilie.teisberg@gmail.com>
2015-03-11 13:02:54 +0100

    asldkfj

--- src/core/Program.java
+++ src/core/Program.java
@@ -1,20 +1,34 @@
 package core;
 
+import java.util.ArrayList;
+
+
 //import database.EventDatabaseHandler;
 //import database.EventDatabaseHandler;
 //import database.GroupDatabaseHandler;
 import database.PersonDatabaseHandler;
 //import database.RoomDatabaseHandler;
+import database.RoomDatabaseHandler;
 
 public class Program {
 	private static PersonDatabaseHandler pdbh = new PersonDatabaseHandler();
 //	private static GroupDatabaseHandler gdbh = new GroupDatabaseHandler();
 //	private static EventDatabaseHandler edbh = new EventDatabaseHandler();
-//	private static RoomDatabaseHandler rdbh = new RoomDatabaseHandler();
+	private static RoomDatabaseHandler rdbh = new RoomDatabaseHandler();
 	
 	public static Person login(String username, String password) {
 		String id = pdbh.login(username, password);
 		Person p = new Person(id);
 		return p;
+	}
+	
+	public static ArrayList<Room> getAllRooms(){
+		ArrayList<String> roomIDs = rdbh.getAllRooms();
+		ArrayList<Room> rooms = new ArrayList<Room>();
+		for (int i = 0; i < roomIDs.size(); i++) {
+			Room r = new Room(roomIDs.get(i));
+			rooms.add(r);
+		}
+		return rooms;
 	}
 }
