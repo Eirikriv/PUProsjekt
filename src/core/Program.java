182648e9abf4310c15b4abb83f28fc402cd974e4
anderhr <anderhr@ts-stud11.idipc.idi.ntnu.no>
2015-03-12 13:33:37 +0100

    rst

--- src/core/Program.java
+++ src/core/Program.java
@@ -3,6 +3,8 @@ package core;
 import java.util.ArrayList;
 
 
+
+import database.GroupDatabaseHandler;
 //import database.EventDatabaseHandler;
 //import database.EventDatabaseHandler;
 //import database.GroupDatabaseHandler;
@@ -12,8 +14,8 @@ import database.RoomDatabaseHandler;
 
 public class Program {
 	private static PersonDatabaseHandler pdbh = new PersonDatabaseHandler();
-//	private static GroupDatabaseHandler gdbh = new GroupDatabaseHandler();
-//	private static EventDatabaseHandler edbh = new EventDatabaseHandler();
+    private static GroupDatabaseHandler gdbh = new GroupDatabaseHandler();
+    private static EventDatabaseHandler edbh = new EventDatabaseHandler();
 	private static RoomDatabaseHandler rdbh = new RoomDatabaseHandler();
 	
 	public static Person login(String username, String password) {
@@ -31,6 +33,11 @@ public class Program {
 		}
 		return rooms;
 	}
+	
+	public static ArrayList<String> getAllGroups(String username) {
+		ArrayList<String> list = pdbh.getAllGroups(username);
+		return list;
+		}
 	
 	public static ArrayList<Person> getAllPersons() {
 		ArrayList<String> persons = pdbh.getAllPersons();
