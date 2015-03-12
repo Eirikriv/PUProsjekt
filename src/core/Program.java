a96606627bd891d9c3dc1126fab07de5cb192e86
anderhr <anderhr@ts-stud11.idipc.idi.ntnu.no>
2015-03-12 13:34:12 +0100

    a

--- src/core/Program.java
+++ src/core/Program.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 
 
+
+import database.EventDatabaseHandler;
 import database.GroupDatabaseHandler;
 //import database.EventDatabaseHandler;
 //import database.EventDatabaseHandler;
